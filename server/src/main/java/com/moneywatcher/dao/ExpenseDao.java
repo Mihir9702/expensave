6f94169d2cedadccd17044ce4a3de93faa872b82
Mihir Patel <mkpatel9702@gmail.com>
2024-03-29 10:48:43 -0400

    server

--- server/src/main/java/com/moneywatcher/dao/ExpenseDao.java
+++
@@ -1,9 +0,0 @@
-package com.moneywatcher.dao;
-
-import java.util.List;
-
-import com.moneywatcher.model.Expense;
-
-public interface ExpenseDao {
-  List<Expense> getExpensesByUserId(int userId);
-}
