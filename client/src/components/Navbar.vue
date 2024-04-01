23e93061af9ed7de1d0e779cabf05ef9248d731a
Mihir Patel <mkpatel9702@gmail.com>
2024-04-01 13:17:22 -0400

    progress ?

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts"></script>
 <template>
 	<header>
-		<nav class="flex items-center justify-between">
-			<ul class="flex justify-start m-4 gap-4">
+		<nav class="flex items-center justify-end w-full">
+			<!-- <ul class="flex justify-start m-4 gap-4">
 				<li class="text-2xl border-2 px-4 py-2 rounded-sm">
 					<RouterLink to="/">Home</RouterLink>
 				</li>
 				<li class="text-2xl border-2 px-4 py-2 rounded-sm">
 					<RouterLink to="/about">About</RouterLink>
 				</li>
-			</ul>
+			</ul> -->
 			<ul class="flex justify-end m-4 gap-4">
 				<li class="text-2xl border-2 px-4 py-2 rounded-sm">
 					<RouterLink to="/login">Login</RouterLink>
@@ -21,4 +21,3 @@
 		</nav>
 	</header>
 </template>
-<style></style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,5 @@
-<script setup lang="ts">
-import Expenses from '../components/Expenses.vue'
-</script>
-
 <template>
-	<Expenses />
+	<div></div>
 </template>
+
+<script setup lang="ts"></script>
