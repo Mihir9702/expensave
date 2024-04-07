796bd83d0053795d6811822bf49cab1c7d51dc2b
Mihir Patel <mkpatel9702@gmail.com>
2024-04-07 17:13:38 -0400

    swap to pc

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,20 +1,27 @@
 <template>
-  <div>
-    <div v-if="isLoading">
-      <Loader />
-    </div>
-    <div v-else>
-    </div>
-  </div>
+	<div>
+		<div v-if="isLoading">
+			<Loader />
+		</div>
+		<div v-else>
+			<h1>Welcome, {{ username }}</h1>
+		</div>
+	</div>
 </template>
 
 <script>
-import Loader from "../components/Loader.vue";
+import Loader from '../components/Loader.vue'
 
 export default {
-  components: {
-    Loader,
-  },
-};
+	components: {
+		Loader,
+	},
+
+	data() {
+		return {
+			username: localStorage.getItem('username'),
+		}
+	},
+}
 </script>
 <style scoped></style>
--- server/data/schema.sql
+++ server/data/schema.sql
@@ -3,19 +3,19 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS users, expenses;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE expenses (
-    id SERIAL PRIMARY KEY,
-    amount DECIMAL(10,2) NOT NULL,
-    description TEXT,
-    date DATE NOT NULL,
+    expense_id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    amount DECIMAL(10, 2) NOT NULL,
+    description TEXT NOT NULL,
+    date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
 COMMIT TRANSACTION;
